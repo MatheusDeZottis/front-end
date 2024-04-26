d945d108174c95c02e6646f8aa0b5be1515cf2c7
MatheusDeZottis <matheusdezottis@gmail.com>
2024-04-25 22:24:40 -0300

    finalizando o estilo do meu formulario

---
+++ projeto pratico/script.js
@@ -0,0 +1,100 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-nome')
+const sFuncao = document.querySelector('#m-funcao')
+const sSalario = document.querySelector('#m-salario')
+const btnsalvar = document.querySelector('btnSalvae')
+let itens
+let id
+
+function openModal(edir = false, index = 0) {
+    modal.classList.add('active')
+
+    modal.onclick = e => {
+        if (e.target.className.indexOf('modal-container') !== -1) {
+            modal.classList.remove('active')
+        }
+    }
+    if (edit) {
+        sNome.value = itens[index].none
+        sFuncao.value = itens[index].funcao
+        sSalario.value = itens[index].salario
+        id = index
+    } else {
+        sNome.value = ''
+        sFuncao.value = ''
+        sSalario.value = ''
+    }
+}
+
+function editItem(index) {
+    openModal(true, index)
+}
+
+function deleteItem(index) {
+    itens.splice(index, 1)
+    seltItensBD()
+    loaditens()
+}
+
+function insertItem(item, index) {
+    let tr = document.createElement('tr')
+    
+    tr.innerHTML = `
+     <td>&{item.nome}</td>
+     <td>&{item.funcao}</td>
+     <td>&{item.salario}</td>
+     <td class="acao">
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></button>
+    </td>
+     <td class='bx bx-edit'></i></button>
+     
+
+    <td class="acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i>< button>
+    </td>
+     `
+    tbody.appendChild(tr)
+}
+
+btnsalvar.onclick = e => {
+
+    if (sNome.value == '' || sFuncao.value == '' || sSalario.value == '') {
+        return
+    }
+
+
+    e.preventDefault();
+
+    if (id !== undefined) {
+        itens[id].nome = sNome.value
+        itens[id].funcao = sFuncao.value
+        itens[id].salario = sSalario.value
+
+    } else {
+        itens.push({ 'nome': sNome.value, 'funcao': sFuncao.value, 'salario': sSalario.value })
+    }
+
+    setItensBD()
+
+    modal.classList.remove('active')
+    loaditens()
+    id = undefined
+
+}
+
+function loaditens() {
+    itens = getItensBD()
+    tbody.innerHTML = ''
+    itens.forEach((item, index) => {
+        insertItem(item, index)
+    })
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loaditens()
+
+
