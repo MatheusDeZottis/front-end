fb0611449822c326105eec8ebf8d3332cbf38583
MatheusDeZottis <matheusdezottis@gmail.com>
2024-04-26 00:57:34 -0300

    tratando erros

--- projeto pratico/script.js
+++ projeto pratico/script.js
@@ -1,6 +1,6 @@
 const modal = document.querySelector('.modal-container')
 const tbody = document.querySelector('tbody')
-const sNome = document.querySelector('#m-nome')
+const sNome = document.querySelector('#m-nome') 
 const sFuncao = document.querySelector('#m-funcao')
 const sSalario = document.querySelector('#m-salario')
 const btnsalvar = document.querySelector('#brnsalvar') 
